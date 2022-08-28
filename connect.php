cb945327fb3d53319c8a33f9a1cd3062801044ed
waheedHnblaas <waheed.hanblaas@gmail.com>
2022-08-28 21:33:55 +0300

    init

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:host=localhost;dbname=ecommmerce";
+$dsn = "mysql:host=localhost;dbname=ecommercecourse";
 $user = "root";
-$pass = "";
+$pass = "root";
 $option = array(
    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
 );
--- test.php
+++ test.php
@@ -7,6 +7,6 @@ $data = array(
 "users_name" => "wael",
 "users_email" => "wael@gmail.com",
 "users_phone" => "324234",
-"users_verfiycode" => "3243243",       
+"users_verifiycode" => "3243243",       
 );
 $count = insertData($table , $data);
